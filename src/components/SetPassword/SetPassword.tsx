a3bbfe208121d3c5fc7401781275ea3812fade7e
Ashleigh Liu <ashleigh9302@gmail.com>
2025-01-09 02:50:13 -0500

    salesbricks

--- src/components/SetPassword/SetPassword.tsx
+++ src/components/SetPassword/SetPassword.tsx
@@ -1,4 +1,11 @@
-import { Text, Button, Center, Box, Fieldset, PasswordInput } from '@mantine/core';
+import {
+  Text,
+  Button,
+  Center,
+  Box,
+  Fieldset,
+  PasswordInput,
+} from "@mantine/core";
 
 type SetPasswordProps = {
   next: () => void;
@@ -15,36 +22,59 @@ export function SetPassword(props: SetPasswordProps) {
         </Box>
       </Center>
       <Center>
-        <Fieldset legend={<Text size="xs">Password</Text>} radius="sm" pt={0} pb={10} mt={5}>
+        <Fieldset
+          legend={<Text size="xs">Password</Text>}
+          radius="sm"
+          pt={0}
+          pb={10}
+          mt={5}
+        >
           <PasswordInput
             mt={10}
             w={500}
             styles={{
               input: {
-                border: 'none',
+                border: "none",
               },
             }}
           />
         </Fieldset>
       </Center>
       <Center>
-        <Fieldset legend={<Text size="xs">Confirm Password</Text>} radius="sm" pt={0} pb={10} mt={5}>
+        <Fieldset
+          legend={<Text size="xs">Confirm Password</Text>}
+          radius="sm"
+          pt={0}
+          pb={10}
+          mt={5}
+        >
           <PasswordInput
             mt={10}
             w={500}
             styles={{
               input: {
-                border: 'none',
+                border: "none",
               },
             }}
           />
         </Fieldset>
       </Center>
       <Center>
-        <Button mt={20} onClick={next} w={540}>CONTINUE</Button>
+        <Button
+          mt={20}
+          onClick={() => {
+            window.location.href =
+              "https://app.salesbricks.com/products/needful-subscription/new?sku=4f7f18f9-7952-4b73-a543-b8bb42a46a61&locked=yes";
+          }}
+          w={540}
+        >
+          CONTINUE
+        </Button>
       </Center>
       <Center>
-        <Button mt={10} variant='outline' w={540}>BACK TO LOGIN</Button>
+        <Button mt={10} variant="outline" w={540}>
+          BACK TO LOGIN
+        </Button>
       </Center>
     </form>
   );
