64359d999a30209ba1963003cc198c48d0bb1761
Ashleigh Liu <ashleigh9302@gmail.com>
2025-01-22 13:18:05 -0500

    trial copy update

--- src/components/SignUpMessage/SignUpMessage.tsx
+++ src/components/SignUpMessage/SignUpMessage.tsx
@@ -1,7 +1,7 @@
-import { Box, Button, Flex, Image, Stack, Text, Title } from '@mantine/core';
-import Clipboard from '../../assets/Clipboard.svg';
-import { Bullet } from '../Bullet/Bullet';
-import { useNavigate } from 'react-router-dom';
+import { Box, Button, Flex, Image, Stack, Text, Title } from "@mantine/core";
+import Clipboard from "../../assets/Clipboard.svg";
+import { Bullet } from "../Bullet/Bullet";
+import { useNavigate } from "react-router-dom";
 
 type SignUpMessageProps = {
   type: null | string;
@@ -13,22 +13,22 @@ export function SignUpMessage(props: SignUpMessageProps) {
   const navigate = useNavigate();
 
   return (
-    <Box style={{ height: '100vh' }}>
+    <Box style={{ height: "100vh" }}>
       <Flex gap="md" justify="flex-end" m={20}>
         <Button
           h={45}
           radius="sm"
           variant="outline"
-          onClick={() => navigate('/login')}
-          style={{ backgroundColor: 'white' }}
+          onClick={() => navigate("/login")}
+          style={{ backgroundColor: "white" }}
         >
           Log in
         </Button>
         <Button
           h={45}
           radius="md"
-          onClick={() => navigate('/signup')}
-          style={{ borderColor: 'white' }}
+          onClick={() => navigate("/signup")}
+          style={{ borderColor: "white" }}
         >
           Contact
         </Button>
@@ -36,21 +36,35 @@ export function SignUpMessage(props: SignUpMessageProps) {
       {type && (
         <Box ml={10}>
           <Image src={Clipboard} h={120} w="auto" ml={10} mb={30} />
-          <Text c="#FFFFFF">Get the full Needful meditation experience for</Text>
-          <Title mb={40} style={{ color: '#FFF492' }}>
+          <Text c="#FFFFFF">
+            Get the full Needful meditation experience for
+          </Text>
+          <Title mb={40} style={{ color: "#FFF492" }}>
             $0 today
           </Title>
-          {type === 'team' && (
-            <Title order={3} mb={20} style={{ color: '#FFFFFF' }}>
-              Enjoy all features for $429.00 CAD per month, billed annually after your 30-day trial.
-              Cancel anytime.
+          {type === "team" && (
+            <Title order={3} mb={20} style={{ color: "#FFFFFF" }}>
+              Enjoy all features for $429.00 CAD per month, billed annually
+              after your 14-day trial. Cancel anytime.
             </Title>
           )}
           <Stack>
-            <Bullet text={'Unlimited access to premium features of the Needful app'} alternate />
-            <Bullet text={'Seamless sync between mobile app and web platform'} alternate />
-            <Bullet text={'Option to integrate with Slack, Asana, and Notion'} alternate />
-            <Bullet text={'24/7 support and resources for optimal adoption'} alternate />
+            <Bullet
+              text={"Unlimited access to premium features of the Needful app"}
+              alternate
+            />
+            <Bullet
+              text={"Seamless sync between mobile app and web platform"}
+              alternate
+            />
+            <Bullet
+              text={"Option to integrate with Slack, Asana, and Notion"}
+              alternate
+            />
+            <Bullet
+              text={"24/7 support and resources for optimal adoption"}
+              alternate
+            />
           </Stack>
         </Box>
       )}
