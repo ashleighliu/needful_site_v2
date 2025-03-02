ecd838d04050718e5ecac090ff2f52c93e422376
Ashleigh Liu <ashleigh9302@gmail.com>
2025-03-02 17:53:49 -0500

    update links on homepage

--- src/components/DownloadButtons/DownloadButtons.tsx
+++ src/components/DownloadButtons/DownloadButtons.tsx
@@ -10,13 +10,32 @@ export function DownloadButtons() {
     <Group mt="md">
       {/* App Store button with QR Code */}
       <Flex align="center" direction="row" gap="xs">
-        <Image src={AppStore} onClick={() => {}} h={50} />
+        <Image
+          src={AppStore}
+          onClick={() =>
+            window.open(
+              "https://apps.apple.com/ca/app/needful-meditate-achieve/id6737121769",
+              "_blank"
+            )
+          }
+          h={50}
+        />
         <Image src={AppStoreQR} alt="QR Code for App Store" h={50} />
       </Flex>
 
       {/* Google Play button with QR Code */}
       <Flex align="center" direction="row" gap="xs">
-        <Image src={GooglePlay} onClick={() => {}} h={50} />
+        <Image
+          src={GooglePlay}
+          onClick={() =>
+            window.open(
+              "https://play.google.com/store/apps/details?id=com.needful_app",
+              "_blank"
+            )
+          }
+          h={50}
+          style={{ cursor: "pointer" }}
+        />
         <Image src={GooglePlayQR} alt="QR Code for Google Play" h={50} />
       </Flex>
     </Group>
