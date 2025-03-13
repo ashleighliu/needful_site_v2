0b78e32a38064f0f2fa9b38c6977158465591d3d
Ashleigh Liu <ashleigh9302@gmail.com>
2025-03-13 02:23:19 -0400

    Remove individual signup flow

--- src/components/IndividualSignUp/IndividualSignUp.tsx
+++ src/components/IndividualSignUp/IndividualSignUp.tsx
@@ -1,217 +1,56 @@
-import { Dispatch, SetStateAction, useState } from "react";
-import {
-  Group,
-  Text,
-  TextInput,
-  Radio,
-  Button,
-  Fieldset,
-  Center,
-  Grid,
-  Select,
-  NumberInput,
-  Image,
-} from "@mantine/core";
-import { useForm } from "@mantine/form";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import Apple from "../../assets/Apple.svg";
-import FacebookBlue from "../../assets/FacebookBlue.svg";
-import Google from "../../assets/Google.svg";
-import classes from "./IndividualSignUp.module.css";
+import { Center, Group, Image, Text, Stack, Grid } from "@mantine/core";
 import { useNavigate } from "react-router-dom";
-import { auth } from "../../services/firebase";
+import AppStore from "../../assets/AppStore.svg";
+import GooglePlay from "../../assets/GooglePlay.svg";
+import QRCode from "../../assets/QRCode.svg";
+import classes from "./IndividualSignUp.module.css";
 
-type SignUpFormFields = {
-  email: string;
-};
-
-type IndividualSignUpProps = {
-  next: () => void;
-  email: string;
-  setEmail: Dispatch<SetStateAction<string>>;
-};
-
-export function IndividualSignUp(props: IndividualSignUpProps) {
-  const { next, email, setEmail } = props;
-
-  const [gender, setGender] = useState<string | null>(null);
+export function IndividualSignUp() {
   const navigate = useNavigate();
-  const form = useForm({
-    initialValues: {
-      email: "",
-    },
-  });
-
-  const [formError, setFormError] = useState(false);
-
-  function signUserUp(values: SignUpFormFields) {
-    createUserWithEmailAndPassword(auth, values.email, "fixedpassword1")
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        // If sign-up is successful, call the next function
-        next();
-      })
-
-      .catch(() => {
-        setFormError(true);
-      });
-  }
 
   return (
-    <form onSubmit={form.onSubmit((values) => signUserUp(values))}>
-      <Grid mt={10}>
-        <Grid.Col span={6}>
-          <TextInput
-            placeholder="First Name"
-            radius="sm"
-            styles={{
-              input: {
-                height: "50px",
-              },
-            }}
-            w={250}
-          />
+    <Stack align="center" gap="xl" mt={40}>
+      <Text size="xl" fw={500}>
+        Download our mobile app to get started
+      </Text>
+      <Grid align="center" justify="center" w="100%" px={100}>
+        <Grid.Col offset={2} span={3}>
+          <Center>
+            <Image
+              src={QRCode}
+              alt="Scan QR Code"
+              style={{ width: 180, height: 180 }}
+            />
+          </Center>
         </Grid.Col>
-        <Grid.Col span={6}>
-          <TextInput
-            placeholder="Last Name"
-            radius="sm"
-            styles={{
-              input: {
-                height: "50px",
-              },
-            }}
-            w={250}
-          />
+        <Grid.Col span={7}>
+          <Stack gap="md" pl={60}>
+            <Image
+              src={AppStore}
+              alt="Download on App Store"
+              onClick={() =>
+                window.open("https://testflight.apple.com/join/NCTUpPCH")
+              }
+              style={{ cursor: "pointer", width: 200 }}
+            />
+            <Image
+              src={GooglePlay}
+              alt="Get it on Google Play"
+              onClick={() =>
+                window.open(
+                  "https://play.google.com/store/apps/details?id=com.needful_app"
+                )
+              }
+              style={{ cursor: "pointer", width: 200 }}
+            />
+          </Stack>
         </Grid.Col>
       </Grid>
-      <Fieldset
-        legend={<Text size="xs">Email Address</Text>}
-        radius="sm"
-        pt={0}
-        pb={10}
-        mt={5}
-      >
-        <TextInput
-          placeholder="Email Address"
-          key={form.key("email")}
-          {...form.getInputProps("email")}
-          onChange={(event) => {
-            form.setFieldValue("email", event.currentTarget.value);
-            setEmail(event.currentTarget.value);
-          }}
-          variant="unstyled"
-          error={
-            formError
-              ? "This email address is already in use. Please log in instead."
-              : false
-          }
-        />
-      </Fieldset>
-      <Radio.Group value={gender} onChange={setGender} mt={5}>
-        <Center>
-          <Grid align="flex-end">
-            <Grid.Col span={6}>
-              <Text c="#555555" mb={5} size="xs">
-                Gender
-              </Text>
-              <Radio.Card className={classes.root} radius="sm" value="team">
-                <Group wrap="nowrap" align="flex-start">
-                  <Radio.Indicator />
-                  <Center>
-                    <Text className={classes.label}>Female</Text>
-                  </Center>
-                </Group>
-              </Radio.Card>
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Radio.Card
-                className={classes.root}
-                radius="sm"
-                value="individual"
-              >
-                <Group wrap="nowrap" align="flex-start">
-                  <Radio.Indicator />
-                  <Center>
-                    <Text className={classes.label}>Male</Text>
-                  </Center>
-                </Group>
-              </Radio.Card>
-            </Grid.Col>
-          </Grid>
-        </Center>
-      </Radio.Group>
-      <Grid align="flex-end" mt={5}>
-        <Grid.Col span={6}>
-          <Fieldset
-            legend={<Text size="xs">Country</Text>}
-            radius="sm"
-            pt={0}
-            pb={10}
-          >
-            <Select
-              placeholder="Country"
-              data={["Canada", "United States"]}
-              variant="unstyled"
-            />
-          </Fieldset>
-        </Grid.Col>
-        <Grid.Col span={6}>
-          <Fieldset
-            legend={<Text size="xs">Phone #</Text>}
-            radius="sm"
-            pt={0}
-            pb={10}
-          >
-            <NumberInput
-              placeholder="Phone #"
-              prefix="+1 "
-              hideControls
-              variant="unstyled"
-              allowNegative={false}
-              allowDecimal={false}
-            />
-          </Fieldset>
-        </Grid.Col>
-      </Grid>
-      <Center>
-        <Center>
-          <Button mt={20} radius="sm" fullWidth type="submit">
-            CONTINUE
-          </Button>
-        </Center>
-      </Center>
-      <Center>
-        <Button
-          mt={10}
-          radius="sm"
-          variant="outline"
-          fullWidth
-          onClick={() => navigate("/login")}
-        >
-          BACK TO LOGIN
-        </Button>
-      </Center>
-      <Center>
-        <Text className={classes.divider}>
-          <Text
-            inherit
-            component="span"
-            className={classes.dividerText}
-            fz="sm"
-          >
-            or continue with
-          </Text>
-        </Text>
-      </Center>
-      <Center>
-        <Group gap="xl">
-          <Image src={Apple} onClick={() => {}} />
-          <Image src={FacebookBlue} onClick={() => {}} />
-          <Image src={Google} onClick={() => {}} />
-        </Group>
-      </Center>
-    </form>
+      <Text c="dimmed" size="sm" ta="center">
+        Our mobile app provides the best experience for individual users.
+        <br />
+        Please download the app to continue.
+      </Text>
+    </Stack>
   );
 }
